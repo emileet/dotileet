2b746ca720cdf29c9bba8bc92fcec7e8e622812b
Emily Maré (emileet) <dev@emi.gay>
2023-09-20 14:49:31 +1000

    chore: clean up
    Signed-off-by: Emily Maré (emileet) <dev@emi.gay>

--- flake.nix
+++ flake.nix
@@ -13,7 +13,6 @@
 
   outputs = { self, nixpkgs, impermanence, font-sf-mono }: {
     nixosConfigurations = (import ./hosts {
-      inherit (nixpkgs) lib;
       inherit nixpkgs impermanence font-sf-mono;
     });
   };
