cc50b67216a79160fcd3a6e381b556b9512b200d
Emily Maré (emileet) <dev@emi.gay>
2023-09-21 00:14:31 +1000

    feat(hardware): add network bridge
    Signed-off-by: Emily Maré (emileet) <dev@emi.gay>

--- hosts/nix/hardware/default.nix
+++ hosts/nix/hardware/default.nix
@@ -15,8 +15,16 @@
   };
 
   networking = {
-    useDHCP = lib.mkDefault true;
     networkmanager.enable = true;
+    interfaces = {
+      enp69s0.useDHCP = true;
+      br0.useDHCP = true;
+    };
+    bridges = {
+      "br0" = {
+        interfaces = [ "enp69s0" ];
+      };
+    };
     firewall = {
       # allowedTCPPorts = [ ... ];
       # allowedUDPPorts = [ ... ];
