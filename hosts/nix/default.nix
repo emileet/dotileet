0f6fcbd92d6e2b38035796b741d5af4372d285da
Emily Maré (emileet) <dev@emi.gay>
2023-09-21 21:45:20 +1000

    feat(service): init liquidctl on boot
    Signed-off-by: Emily Maré (emileet) <dev@emi.gay>

--- hosts/nix/default.nix
+++ hosts/nix/default.nix
@@ -60,17 +60,33 @@
   };
 
   systemd = {
-    user.services.polkit-gnome-authentication-agent-1 = {
-      description = "polkit-gnome-authentication-agent-1";
-      wantedBy = [ "graphical-session.target" ];
-      wants = [ "graphical-session.target" ];
-      after = [ "graphical-session.target" ];
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        TimeoutStopSec = 10;
-        RestartSec = 1;
+    services = {
+      init-liquidctl = {
+        description = "initialise liquidctl";
+        wantedBy = [ "default.target" ];
+        serviceConfig = {
+          Type = "oneshot"; # like all banger yuris :')
+          ExecStart = [
+            "${pkgs.liquidctl}/bin/liquidctl -m kraken initialize"
+            "${pkgs.liquidctl}/bin/liquidctl -m kraken set pump speed 20 80 30 80 40 90 50 100"
+            "${pkgs.liquidctl}/bin/liquidctl -m kraken set fan speed 20 40 30 40 35 60 40 60 50 80 60 100"
+          ];
+        };
+      };
+    };
+    user.services = {
+      polkit-gnome-authentication-agent-1 = {
+        description = "polkit-gnome-authentication-agent-1";
+        wantedBy = [ "graphical-session.target" ];
+        wants = [ "graphical-session.target" ];
+        after = [ "graphical-session.target" ];
+        serviceConfig = {
+          Type = "simple";
+          ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+          Restart = "on-failure";
+          TimeoutStopSec = 10;
+          RestartSec = 1;
+        };
       };
     };
   };
