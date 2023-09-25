e67026dd2c53f50a044da595528e7273194b997a
Emily Maré (emileet) <dev@emi.gay>
2023-09-26 00:00:16 +1000

    feat(lightdm): configure greeter
    Signed-off-by: Emily Maré (emileet) <dev@emi.gay>

--- hosts/nix/hardware/display.nix
+++ hosts/nix/hardware/display.nix
@@ -15,7 +15,30 @@
 
     displayManager = {
       defaultSession = "none+i3";
-      lightdm.enable = true;
+      lightdm = {
+        greeters.mini = {
+          extraConfig = ''
+            [greeter]
+            show-password-label = false
+            password-input-width = 28
+            [greeter-theme]
+            background-color = "#000000"
+            window-color = "#1e1e2e"
+            border-color = "#11111b"
+            border-width = 1px
+            password-background-color = "#313244"
+            password-border-color = "#11111b"
+            password-border-width = 1px
+            password-color = "#cdd6f4"
+            error-color = "#f38ba8"
+            text-color = "#cdd6f4"
+          '';
+          user = "emileet";
+          enable = true;
+        };
+        background = "/storage/pictures/wallpapers/nightsky-2560.jpg";
+        enable = true;
+      };
       setupCommands = ''
         LEFT='DisplayPort-1'
         RIGHT='DisplayPort-0'
@@ -31,7 +54,7 @@
     libinput.enable = false;
     layout = "us";
 
-    autorun = false;
+    autorun = true;
     enable = true;
   };
 }
