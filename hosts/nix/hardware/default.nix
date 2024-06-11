1a3298d936d109c5b3fa787531cd632d3ada2da1
Emily Maré (emileet) <dev@emi.gay>
2024-06-11 13:21:49 +1000

    chore: bump inputs
    Signed-off-by: Emily Maré (emileet) <dev@emi.gay>

--- hosts/nix/hardware/default.nix
+++ hosts/nix/hardware/default.nix
@@ -53,7 +53,7 @@
 
     kernelPackages = pkgs.linuxPackages_zen;
     kernelPatches = [{
-      patch = /nix/patches/linux/linux-vmi-6.8.patch;
+      patch = /nix/patches/linux/linux-vmi-6.9.3.patch;
       name = "virtual machine introspection";
       extraConfig = ''
         EVDEV_MIRROR y
