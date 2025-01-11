e70d2a923511ff10a405c245b3225a02192a61a2
Emily Maré (emileet) <dev@emi.gay>
2025-01-11 21:17:19 +1100

    refactor: make nicer idk
    Signed-off-by: Emily Maré (emileet) <dev@emi.gay>

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,8 +3,8 @@ args@{ ... }:
   nixpkgs.config.allowUnfree = true;
   imports = [
     (import ./overlays args)
+    ./include/memflow
+    ./include/kvmfr
     ./system.nix
-    ./memflow
-    ./kvmfr
   ];
 }
--- pkgs/overlays/include.nix
+++ pkgs/overlays/include.nix
@@ -1,6 +1,6 @@
 src-vban: src-vkcapture:
 (final: prev: {
-  vban = prev.callPackage ../vban { inherit src-vban; };
-  obs-vkcapture-kms = prev.callPackage ../vkcapture { inherit src-vkcapture; };
-  obs-kvmfr = prev.callPackage ../kvmfr/obs { };
+  vban = prev.callPackage ../include/vban { inherit src-vban; };
+  obs-vkcapture-kms = prev.callPackage ../include/vkcapture { inherit src-vkcapture; };
+  obs-kvmfr = prev.callPackage ../include/kvmfr/obs { };
 })
