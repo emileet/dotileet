b62db8d613264aa88277ee02754eee21d0183426
Emily Maré (emileet) <dev@emi.gay>
2026-05-17 10:38:30 +1000

    chore: bump inputs
    Signed-off-by: Emily Maré (emileet) <dev@emi.gay>

--- modules/services/ollama/default.nix
+++ modules/services/ollama/default.nix
@@ -11,7 +11,7 @@ in
 {
   config = mkIf cfg.enable {
     services = {
-      open-webui.enable = false;
+      open-webui.enable = true;
       ollama = {
         package = pkgs.ollama-cuda;
         home = "/storage/var/lib/ollama";
