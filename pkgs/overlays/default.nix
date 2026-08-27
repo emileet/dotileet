0ff4de06fb83bf55aeb3b60e6d466b91ef65737d
Emily Maré (emileet) <dev@emi.gay>
2026-08-27 14:27:45 +1000

    refactor: clean up
    Signed-off-by: Emily Maré (emileet) <dev@emi.gay>

--- pkgs/overlays/default.nix
+++ pkgs/overlays/default.nix
@@ -1,25 +1,17 @@
 { inputs, ... }:
-let
-  inherit (inputs)
-    nixpkgs-master
-    nvidia-patch
-    quickshell
-    waybar
-    ;
-in
 {
   nixpkgs.overlays = [
     (import ./include.nix { inherit inputs; })
     (import ./modify.nix { inherit inputs; })
     (import ./secret.nix { inherit inputs; })
     (final: prev: {
-      master = import nixpkgs-master {
+      master = import inputs.nixpkgs-master {
         system = final.stdenv.hostPlatform.system;
         inherit (final) config;
       };
     })
-    nvidia-patch.overlays.default
-    quickshell.overlays.default
-    waybar.overlays.default
+    inputs.nvidia-patch.overlays.default
+    inputs.quickshell.overlays.default
+    inputs.waybar.overlays.default
   ];
 }
