257140a6a88f6013231fb53b9428936c4b792cd6
Emily Maré (emileet) <dev@emi.gay>
2026-09-06 05:26:57 +1000

    feat: add quickshell user service
    Signed-off-by: Emily Maré (emileet) <dev@emi.gay>

--- home/services/wm/hyprland/default.nix
+++ home/services/wm/hyprland/default.nix
@@ -1,11 +1,13 @@
 {
   lib,
   pkgs,
+  config,
   osConfig,
   ...
 }:
 with lib;
 let
+  quickshellConfig = "${config.xdg.configHome}/quickshell/classic";
   hyprlandEnabled = osConfig.programs.hyprland.enable;
 in
 {
@@ -15,21 +17,17 @@ in
       hyprls
     ];
     systemd.user.services = {
+      quickshell = {
+        Unit.Description = "Flexible toolkit for making desktop shells with QtQuick";
+        Service.ExecStart = "${pkgs.quickshell}/bin/qs -p ${quickshellConfig}";
+      };
       hyprpaper = {
         Unit.Description = "Wayland wallpaper utility";
-        Service = {
-          Type = "simple";
-          Restart = "no";
-          ExecStart = "${pkgs.hyprpaper}/bin/hyprpaper";
-        };
+        Service.ExecStart = "${pkgs.hyprpaper}/bin/hyprpaper";
       };
       waybar = {
         Unit.Description = "Highly customizable Wayland bar";
-        Service = {
-          Type = "simple";
-          Restart = "no";
-          ExecStart = "${pkgs.waybar}/bin/waybar";
-        };
+        Service.ExecStart = "${pkgs.waybar}/bin/waybar";
       };
     };
   };
