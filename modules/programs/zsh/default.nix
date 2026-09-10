7c2a927929634a614f21d6e41c67fd09e8c58f0c
Emily Maré (emileet) <dev@emi.gay>
2026-09-11 06:10:19 +1000

    feat: update zsh config
    Signed-off-by: Emily Maré (emileet) <dev@emi.gay>

--- modules/programs/zsh/default.nix
+++ modules/programs/zsh/default.nix
@@ -8,8 +8,9 @@ in
     programs = {
       zsh = {
         shellAliases = {
-          ela = "eza -lagh --icons --group-directories-first";
-          el = "eza -lgh --icons --group-directories-first";
+          a2c = ", aria2c --max-connection-per-server=16 --min-split-size=1M --split=16 --max-concurrent-downloads=5 --file-allocation=falloc --summary-interval=1 --human-readable=true";
+          la = "eza -lagh --icons --group-directories-first";
+          ls = "eza -lgh --icons --group-directories-first";
         };
       };
     };
